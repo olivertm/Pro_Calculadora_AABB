d4611064a63db3cb3e934136e792d771601f3a61
Oliver Tacoronte <olivertm@gmail.com>
2022-02-08 16:27:50 +0000

    añadida funcion porcentaje

--- src/main/java/com/mycompany/pro_calculadora_aabb/Calculadora.java
+++ src/main/java/com/mycompany/pro_calculadora_aabb/Calculadora.java
@@ -9,5 +9,12 @@ package com.mycompany.pro_calculadora_aabb;
  * @author olivert
  */
 public class Calculadora {
-    
+    public static int Porcentaje(int numero, int por) {
+        return (numero*por)/100;
+    }
+        
+    public static void main(String args[])
+    {
+        System.out.println("30% de 100 es = "+ Porcentaje(100,30));
+    }
 }
